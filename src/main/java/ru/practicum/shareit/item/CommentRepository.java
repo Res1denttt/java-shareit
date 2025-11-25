e85ccc488e4fe4c6a020de566958d1a3ce3b68ea
Dmitry Guskov <guskovd@internet.ru>
2025-11-25 03:21:02 +0300

    Code style

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/CommentRepository.java
@@ -1,6 +1,8 @@
-package ru.practicum.shareit.item.model;
+package ru.practicum.shareit.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 
--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -11,7 +11,6 @@ import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.OwnerItemDto;
 import ru.practicum.shareit.item.model.Comment;
-import ru.practicum.shareit.item.model.CommentRepository;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserRepository;
