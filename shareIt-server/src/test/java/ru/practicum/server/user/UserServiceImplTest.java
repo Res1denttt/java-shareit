95a8f822531f26891a76fae67a13012b0143b3d1
Dmitry Guskov <guskovd@internet.ru>
2025-12-06 17:16:47 +0300

    CheckStyle 2

--- shareIt-server/src/test/java/ru/practicum/server/user/UserServiceImplTest.java
+++ shareIt-server/src/test/java/ru/practicum/server/user/UserServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.server.user;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
